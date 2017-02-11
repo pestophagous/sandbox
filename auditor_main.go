304f64d1008146f2909c81d21f18fdf4c181dbc6
someone <someone@localhost>
2017-02-11 15:30:12 -0800

    Again probing the limits of goreportcard (as of Feb 2017)

--- auditor_main.go
+++ auditor_main.go
@@ -6,20 +6,21 @@ import (
 
 func main() {
 	s := ", x"
-	// coment with a mispeling in it on purpose.
+	// misspelled words not found by reportcard (Feb 2017): coment, mispeling
 	fmt.Println("auditor_main", s)
 
 	{
-		e, s := "e", "shadow"
+		e, s := "e", "shadow" // s is possible accidental shadow but not caught
 		fmt.Println(s)
-		fmt.Printf("%s %d\n", e, 4)
+		fmt.Printf("%s %s %d\n", e, s, 4)
 	}
 }
 
+// reportcard must not be auditing docu-comment style
 // this funcion is badly doc'ed and not used
 func unreached() {
 	if true {
 	} else {
-		fmt.Println("unreached")
+		fmt.Println("unreached") // this is also not flagged
 	}
 }
