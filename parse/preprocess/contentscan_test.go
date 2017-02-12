246e8bcca34cb2f0b0504cc1eee466cdcac2135c
someone <someone@localhost>
2017-02-12 15:44:18 -0800

    Add an Example func to tests that shows up in godoc.

--- parse/preprocess/contentscan_test.go
+++ parse/preprocess/contentscan_test.go
@@ -9,6 +9,9 @@ import (
 	localtests "github.com/practicum/sandbox/testing"
 )
 
+var sampleTxtFilepath string = localtests.
+	DataAssetFullPath("january.txt", reflectionToy{})
+
 // reflectionToy is a dummy type created in the preprocess package as a trick to
 // retrieve the package name. http://stackoverflow.com/a/25263604/10278
 type reflectionToy struct{}
@@ -17,14 +20,28 @@ func TestScan(t *testing.T) {
 	datapath := localtests.DataAssetFullPath("january.txt", reflectionToy{})
 	iterator := NewContentIterator(datapath)
 
-	line, err := iterator()
+	_, err := iterator()
 
 	for err == nil {
-		line, err = iterator()
-		fmt.Println(line)
+		_, err = iterator()
 	}
 
 	if err != io.EOF {
 		t.Error(err)
+	}
+}
+
+func ExampleNewContentIterator() {
+	iterator := NewContentIterator(sampleTxtFilepath)
+
+	line, err := iterator()
+
+	for err == nil {
+		fmt.Println(line)
+		line, err = iterator()
+	}
+
+	if err != io.EOF {
+		panic(fmt.Sprintf("Error while iterating over %s.", sampleTxtFilepath))
 	}
 }
